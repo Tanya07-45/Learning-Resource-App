3e23ca7f530e616464f89397659324f37eaf056e
Tanya Shukla <stanya97@gmail.com>
2021-10-08 16:01:16 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <ul>
+        <learning-resource
+         v-for="res in storedResources" 
+         :key="res.id" 
+         :title="res.title"
+         :description="res.description"
+         :link="res.link"
+         ></learning-resource>
+         <!-- here we are feeding the values in props in learning resource file -->
+    </ul>
+</template>
+<script>
+ import LearningResource from './components/learningResourse/LearningResource.vue'
+  export default{
+    components: {
+     LearningResource
+    },
+    data(){
+        return{
+            storedResources:[
+                {
+                    id: 'official-guide',
+                    title:'Official Guide',
+                    description:'the official documentation',
+                    link:'https://vuejs.org'
+                },//here every object can be a one learning resource
+                {
+                        id: 'google',
+                        title:'Google',
+                        description:'Learn to Google....',
+                        link:'https://google.org'
+                },
+            ]
+        };
+    },
+};
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
